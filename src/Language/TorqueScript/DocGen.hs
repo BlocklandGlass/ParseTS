c3dda8be3e0434af51ee4e3f3a3834bb3d1ab5f0
Teo Klestrup Röijezon <teozkr@gmail.com>
2015-08-07 20:35:27 +0200

    Rewrote all Hamlet templates using Blaze combinators
    Template Haskell is a nice tool, but ultimately it slows down the
    compilation too much, and breaks too many tools to be a good fit right now.

--- src/Language/TorqueScript/DocGen.hs
+++ src/Language/TorqueScript/DocGen.hs
@@ -3,58 +3,58 @@ module Language.TorqueScript.DocGen where
 import Language.TorqueScript.AST
 import Language.TorqueScript.Analysis
 
-import Text.Hamlet
+import Control.Applicative
+import Control.Monad
+import Text.Blaze.Html5((!), toValue, toMarkup, Html)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
+data DocNode = DocDirectory FilePath [DocNode]
+             | DocFile FilePath [DocNode]
 
 docFunctionId :: Function -> String
 docFunctionId (Function _ name _ _) = "function-" ++ name
 
 docFunction :: WithSourcePos Function -> Html
-docFunction (WithSourcePos _ function@(Function docstring name args _)) = [shamlet|
-<div class="function" id=#{docFunctionId function}>
-    <h3> #{name}
-    $if not $ null args
-        <div>
-            <h4> Arguments
-            <ul>
-                $forall arg <- args
-                    <li> %#{arg}
-    <div>
-        $forall docstringLine <- docstring
-            <p> #{docstringLine}
-|]
+docFunction (WithSourcePos _ function@(Function docstring name args _)) =
+  H.div ! A.class_ "function" ! A.id (toValue $ docFunctionId function) $ do
+    H.h3 $ toMarkup name
+    unless (null args) $
+      H.div $ do
+        H.h4 "Arguments"
+        H.ul $
+          sequence_ $ H.li . toMarkup . ('%' :) <$> args
+    H.div $
+      sequence_ $ toMarkup <$> docstring
 
 docFunctions :: [WithSourcePos Function] -> Html
-docFunctions functions = [shamlet|
-<div>
-    <h2> Functions
-    <ul class="functions">
-        $forall f <- functions
-            <li> #{docFunction f}
-|]
+docFunctions functions =
+  H.div $ do
+    H.h2 "Functions"
+    H.ul ! A.class_ "functions" $
+      sequence_ $ docFunction <$> functions
 
 docPackage :: WithSourcePos Package -> Html
-docPackage (WithSourcePos _ (Package name functions)) = [shamlet|
-<div>
-    <h3> #{name}
-    #{docFunctions functions}
-|]
+docPackage (WithSourcePos _ (Package name functions)) =
+  H.div $ do
+    H.h3 $ toMarkup name
+    docFunctions functions
 
 docPackages :: [WithSourcePos Package] -> Html
-docPackages packages = [shamlet|
-<div>
-    <h2> Packages
-    <ul>
-        $forall package <- packages
-            <li> #{docPackage package}
-|]
+docPackages packages =
+  H.div $ do
+    H.h2 "Packages"
+    H.ul $
+      sequence_ $ docPackage <$> packages
 
 docFile :: String -> [TopLevel] -> Html
-docFile name file = [shamlet|
-<html>
-  <head>
-    <title> ParseTS - #{name}
-  <body>
-    <h1> #{name}
-    #{docFunctions $ allFunctions False file}
-    #{docPackages $ allPackages file}
-|]
+docFile name file =
+  H.html $ do
+    H.head $
+      H.title $ do
+        _ <- "ParseTS - "
+        toMarkup name
+    H.body $ do
+      H.h1 $ toMarkup name
+      docFunctions $ allFunctions False file
+      docPackages $ allPackages file
